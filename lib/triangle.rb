aee2d25383e4ed4b79a40875c8d1f8bf921080bb
Anne Fausto <a.r.fausto@gmail.com>
2019-10-20 02:30:49 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
+  
+  attr_reader :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+
+  def kind
+     sides = [@side1, @side2, @side3]
+     raise TriangleError if (@side1 <= 0 || @side2 <= 0 || @side3 <= 0 ) or (sides.first+sides[1] <= sides.last || sides.first+sides.last <= sides[1] || sides[1]+sides.last <= sides.first) 
+     return :equilateral if sides.first==sides[1] && sides[1] ==sides.last
+     return :isosceles if sides[1] == sides.last || sides.first == sides.last || sides.first == sides[1]
+     return :scalene if sides.uniq.length == 3
+  end 
+     
+  class TriangleError < StandardError
+  end 
+  
+       # checks to see if all sides are greater than 0 and to see if 3rd side is always larger than the other two sides combined
+
   # write code here
 end
